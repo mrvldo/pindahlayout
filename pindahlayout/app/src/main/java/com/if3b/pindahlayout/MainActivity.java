c780da217db5096bd5e4fa04449ec2dfee00eb62
gracekells <mariorivaldo86@gmail.com>
2022-10-03 11:20:31 +0700

    update commit

--- pindahlayout/app/src/main/java/com/if3b/pindahlayout/MainActivity.java
+++ pindahlayout/app/src/main/java/com/if3b/pindahlayout/MainActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button btnpindah;
-
+    EditText etNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Layout A");
 
         btnpindah = findViewById(R.id.btn_pindah);
-
+        etNama = findViewById(R.id.et_nama);
 
         btnpindah.setOnClickListener(new View.OnClickListener() {
             @Override
